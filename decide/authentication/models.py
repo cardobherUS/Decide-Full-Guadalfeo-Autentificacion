a8c1bac17789998e573b5cb4fa30eac19f5ac9d0
jaialodie <jaime.alonso.diez@gmail.com>
2020-12-15 18:09:56 +0100

    Revert "Update models.py"
    This reverts commit 0f8dfc26ac15835ab8ea49bfea0fad7a7030083c.

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,16 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Candidatura(models.Model):
-    nombre = models.TextField()
-    delegadoCentro = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='delegado_centro')
-    representanteDelegadoPrimero = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='representante_primero')
-    representanteDelegadoSegundo = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='representante_segundo')
-    representanteDelegadoTercero = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='representante_tercero')
-    representanteDelegadoCuarto = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='representante_cuarto')
-    representanteDelegadoMaster = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='representante_master')
-
-    def __str__(self):
-        return self.nombre
