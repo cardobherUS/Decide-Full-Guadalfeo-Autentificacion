059cb5595cbe50b3d45f3647195c840f0b95e732
jaialodie <jaime.alonso.diez@gmail.com>
2021-01-14 20:09:57 +0100

    Test Selenium

--- decide/authentication/test_selenium.py
+++ decide/authentication/test_selenium.py
@@ -23,4 +23,18 @@ class TestTestgetuserdetails():
     self.driver.find_element(By.LINK_TEXT, "Login with Google").click()
     self.driver.find_element(By.ID, "profileIdentifier").click()
     self.driver.find_element(By.LINK_TEXT, "jaime").click()
+
+class TestTestCompleteVotingUserDetails():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
+    self.vars = {}
   
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_testCompleteVotingUserDetails(self):
+    self.driver.get("http://localhost:8000/")
+    self.driver.set_window_size(970, 748)
+    self.driver.find_element(By.LINK_TEXT, "Login").click()
+    self.driver.find_element(By.LINK_TEXT, "Login with Linkedin").click()
+    self.driver.find_element(By.LINK_TEXT, "Finish setting your profile here to access the platform").click()
